f6803b45f11509465ed22c57f81ce3fa4601ca11
Ion Stici <ionsticidev@gmail.com>
2025-05-30 11:45:08 +0200

    Finish the bookbot project

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 from stats import get_num_words
 from stats import get_num_of_each_character
+from stats import get_sorted_dict
+import sys
+
+env_vars = sys.argv
+is_env_valid = len(env_vars) > 1
+
+if not is_env_valid :
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text (path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
+filepath = env_vars[1]
+
 def main ():
-    file_contents = get_book_text("./books/frankenstein.txt")
+    file_contents = get_book_text(filepath)
     num_words = get_num_words(file_contents)
 
-    print(f"{num_words} words found in the document")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
 
     num_of_each_char = get_num_of_each_character(file_contents)
+    sorted_list = get_sorted_dict(num_of_each_char)
 
-    print(num_of_each_char)
+    for item in sorted_list:
+        if (item["char"].isalpha()):
+            print(f"{item["char"]}: {item["num"]}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,18 @@ def get_num_of_each_character (text):
             chars[l] = 1
 
     return chars
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_dict (dict):
+    sorted_list = []
+
+    for key, value in dict.items():
+        sorted_list.append({ "char": key, "num": value })
+
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list
+
+
